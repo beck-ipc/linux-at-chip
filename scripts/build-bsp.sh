67fdcfec7c82a9d6c5ca692cc6610460185bc804
Florian Boor <florian.boor@kernelconcepts.de>
2017-03-30 12:07:59 +0200

    Build BSP inside BSP directory

--- scripts/build-bsp.sh
+++ scripts/build-bsp.sh
@@ -6,7 +6,6 @@ set +e
 echo
 echo "LINUX@CHIP BSP Version ${BSPVERSION}"
 echo
-cd yocto-bsp
 . ./oe-init-build-env build-sc145
 echo Building for SC145 in "`pwd`"
 bitbake at-chip-console-image
@@ -14,5 +13,4 @@ echo Building toolchain
 bitbake at-chip-console-image -c populate_sdk
 VM_TOOLCHAIN=$PWD/`ls tmp/deploy/sdk/*.sh`
 echo Toolchain located at $VM_TOOLCHAIN
-cd ..
 echo DONE
